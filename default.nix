442cd6d06488f23cc2a3c1ead794c7fba56923e4
Dan Dart <git@dandart.co.uk>
2023-02-28 18:11:55 +0000

    Nix scaffolding

---
+++ default.nix
@@ -0,0 +1,32 @@
+{
+  nixpkgs ? import <nixpkgs> {},
+  compiler ? "ghc92",
+  haskell-tools ? import (builtins.fetchTarball "https://github.com/danwdart/haskell-tools/archive/master.tar.gz") {
+    nixpkgs = nixpkgs;
+    compiler = compiler;
+  }
+}:
+let
+  gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+  haskell = nixpkgs.pkgs.haskell;
+  haskellPackages = haskell.packages;
+  lib = haskell.lib;
+  tools = haskell-tools compiler;
+  myHaskellPackages = haskellPackages.${compiler}.override {
+    overrides = self: super: rec {
+      digits = lib.dontHaddock (self.callCabal2nix "digits" (gitignore ./.) {});
+    };
+  };
+  shell = myHaskellPackages.shellFor {
+    packages = p: [
+      p.digits
+    ];
+    buildInputs = tools.defaultBuildTools;
+    withHoogle = false;
+  };
+  exe = lib.justStaticExecutables (myHaskellPackages.digits);
+in
+{
+  inherit shell;
+  digits = lib.justStaticExecutables (myHaskellPackages.digits);
+}
