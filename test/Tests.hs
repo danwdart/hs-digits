5555e9155be10256b64918e325374cf3b6263e73
Dan Dart <git@dandart.co.uk>
2023-09-28 18:34:27 +0100

    postpositive qualified + hlint

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,20 +1,20 @@
 module Main where
 
-import           Data.Digits
-import           Test.QuickCheck
-import           Text.Printf
+import Data.Digits
+import Test.QuickCheck
+import Text.Printf
 
 -- | unDigits . digits should be the identity, in any positive base.
 prop_digitsRoundTrip
-    :: Integer -- ^ The integer to test.
-    -> Integer -- ^ The base to use.
-    -> Property
+    ∷ Integer -- ^ The integer to test.
+    → Integer -- ^ The base to use.
+    → Property
 prop_digitsRoundTrip i b = i > 0 ==> b > 0 ==> i == (unDigits b . digits b) i
 
-tests :: [([Char], IO ())]
+tests ∷ [([Char], IO ())]
 tests = [
     ("checkDigitsRoundTrip", quickCheck prop_digitsRoundTrip)]
 
-main :: IO ()
+main ∷ IO ()
 main = mapM_ (\(s,a) -> printf "%-25s: " s >> a) tests
 
