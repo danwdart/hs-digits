89d00da1119a8505f61cf8f40ef7404548c4f182
Dan Dart <git@dandart.co.uk>
2024-11-05 19:22:22 +0000

    lint

--- test/Tests.hs
+++ test/Tests.hs
@@ -6,15 +6,15 @@ import Text.Printf
 
 -- | unDigits . digits should be the identity, in any positive base.
 prop_digitsRoundTrip
-    :: Integer -- ^ The integer to test.
-    -> Integer -- ^ The base to use.
-    -> Property
+    ∷ Integer -- ^ The integer to test.
+    → Integer -- ^ The base to use.
+    → Property
 prop_digitsRoundTrip i b = i > 0 ==> b > 0 ==> i == (unDigits b . digits b) i
 
-tests :: [([Char], IO ())]
+tests ∷ [([Char], IO ())]
 tests = [
     ("checkDigitsRoundTrip", quickCheck prop_digitsRoundTrip)]
 
-main :: IO ()
+main ∷ IO ()
 main = mapM_ (\(s,a) -> printf "%-25s: " s >> a) tests
 
